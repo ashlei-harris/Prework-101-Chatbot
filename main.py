7eb0fa26d37d49f6c07f85e515ed9b1bfa8aad14
ash <ash.louise317@gmail.com>
2025-09-19 23:10:50 +0000

    finished chatbot

--- main.py
+++ main.py
@@ -1,5 +1,58 @@
+def option_menu():
+  print('\n Please select an option')
+  print('1. View pets')
+  print('2. Adopt a pet')
+  print('3. Put a pet up for adoption')
+  print('4. Serach for a pet')
+  print('5. Exit menu')
+
+def selection():
+  option = int(input('\n Please enter the number for your option: '))
+  if option == 1:
+    viewpets()
+  elif option == 2:
+    adopt()
+  elif option == 3:
+    add()
+  elif option == 4:
+    serach()
+  elif option == 5:
+    print(' \n Thank you and goodbye',name,'! Have a nice day!')
+  else:
+    print('invalid option. Try again')
+    option_menu()
+    selection()
+
+def viewpets():
+  print('View pets')
+  action = bool(input('\n Enter to return to main menu: '))
+  if action == True:
+    option_menu()
+    selection()
+def adopt():
+  print('Adopt a pet')
+  action = bool(input('\n Enter to return to main menu: '))
+  if action == True:
+    option_menu()
+    selection()
+def add():
+  print('Add a pet')
+  action = bool(input('\n Enter to return to main menu: '))
+  if action == True:
+    option_menu()
+    selection()
+def serach():
+  print('Serach for pet')
+  action = bool(input('\n Enter to return to main menu: '))
+  if action == True:
+    option_menu()
+    selection()
+
 print('Welcome to the pet adoption chatbot!')
-name = input('Plese enter your name: ')
-age = input('Plese enter your age: ')
+name = input('Please enter your name: ')
+age = input('Please enter your age: ')
 print('\n Hello', name, 'you are', age, 'years old!')
 print('\n How may I help you?')
+
+option_menu()
+selection()
